79e8ed325196508263f915f36074e240cada1b15
Gaurav Kesarwani <gauravkesarwani94@gmail.com>
2021-05-30 22:35:36 +0530

    Final Changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
-const express = require('express')
-require('./db/mongoose') //just connects
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
-
-const app = express()
+const app = require('./app')
 const port = process.env.PORT
-
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
 
 app.get('/', (req,res)=>{
     res.send('goto /user or /tasks for more info')
@@ -17,18 +8,3 @@ app.get('/', (req,res)=>{
 app.listen(port, () =>{
     console.log('Server is up on ' + port)
 })
-
-const Task = require('./models/task')
-const User = require('./models/user')
-
-// const main = async () => {
-//     // const task = await Task.findById('60b131b785b10a206cbccd07')
-//     // await task.populate('owner').execPopulate()
-//     // console.log(task.owner)
-
-//     const user = await User.findById('60b12e9faba9894204e67550')
-//     await user.populate('tasks').execPopulate()
-//     console.log(user.tasks)
-// }
-
-// main()
---
+++ tests/task.test.js
@@ -0,0 +1,45 @@
+const request = require('supertest')
+const app = require('../src/app')
+const Task = require('../src/models/task')
+const { 
+    userOneId, 
+    userOne, 
+    setupDatabase,
+    userTwo,
+    userTwoId,
+    taskOne,
+    taskTwo,
+    taskThree } = require('./fixtures/db')
+
+beforeEach(setupDatabase)
+
+test('Should create task for user', async () => {
+    const response = await request(app)
+        .post('/tasks')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            description: "From my test"
+        })
+        .expect(201)
+    const task = await Task.findById(response.body._id)
+    expect(task).not.toBeNull()
+    expect(task.completed).toEqual(false)
+})
+
+test('All Tasks', async () => {
+    const response = await request(app)
+    .get('/tasks')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .expect(200)
+    expect(response.body.length).toEqual(2)
+})
+
+test('Should not delete other users task', async () => {
+    const response = await request(app)
+    .delete('/tasks')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send(`id=${taskOne._id}`)
+    .expect(404)
+    const task = Task.findById(taskOne._id)
+    expect(task).not.toBeNull()
+})
